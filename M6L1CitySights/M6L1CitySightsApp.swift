94a3f1699eb2ebbf2b46cccac3404567a0c864eb
Todd Farris <cimogo@yahoo.com>
2021-12-02 09:12:56 -0800

    Initial Commit

---
+++ M6L1CitySights/M6L1CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  M6L1CitySightsApp.swift
+//  M6L1CitySights
+//
+//  Created by Todd Farris on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct M6L1CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
